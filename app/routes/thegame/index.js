3f71c76e68743db2077cb2cb397304dff18ac95d
NatanDC <natandc740@gmail.com>
2019-05-05 23:41:59 -0500

    decent

--- app/routes/thegame/index.js
+++ app/routes/thegame/index.js
@@ -4,7 +4,7 @@ import { hash } from 'rsvp';
 export default class ThegameIndexRoute extends Route {
 	model(){
 		return hash({
-			games: this.store.findAll('game'),
+			//games: this.store.findAll('game'),
 			//players: this.store.findAll('user')
           	//plays: this.store.findAll('play')
 		})
